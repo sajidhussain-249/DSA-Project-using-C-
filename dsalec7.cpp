69484218bab974c11829ab8f52b9790b9a3768b3
sajidhussain-249 <sajidhussain@lpu.in>
2024-07-17 14:46:59 +0530

    Delete dsalec7.cpp

--- dsalec7.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a,b,c,d,e;
-    cout<<"give 5 integers as input: "<<endl;
-    cin>>a>>b>>c>>d>>e;
-    cout<<"The integers in reverse order are "<<e<<d<<c<<b<<a;
-    return 0;
-}
